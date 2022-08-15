e8cccac4deaab74a9ca2ec6e8d6fa841d79da7b8
pedram noohi <pedram.noohi100@gmail.com>
2022-08-15 17:07:14 +0200

    Initial commit

---
+++ solver/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about solver"""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Pedram Noohi"
+__email__ = "pedram.noohi100@gmail.com"
+__version__ = "0.1.0"
